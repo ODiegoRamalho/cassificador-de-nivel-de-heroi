05064075f4d53189d410ccf37c5b365c2f5eb38c
ODiegoRamalho <professordiegoramalho@gmail.com>
2024-05-28 19:16:52 -0300

    funcoes.js

---
+++ funcoes.js
@@ -0,0 +1,23 @@
+ligarSom()
+
+function ligarSom() {
+    console.log("ligar tocador")
+    tocarCavaleirosDoForro() // funcao dentro de funcao
+}
+
+function tocarCavaleirosDoForro () {
+    console.log("tocar Cavaleiros do Forró")
+}
+
+/* OBS 
+NOME
+    não começar com números
+    não dar espaços
+    no nome usar verbos 
+
+função main
+    função que cham outras funções
+
+Métodos também são funções
+
+*/
